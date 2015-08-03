12e144ba93fd0fa55127428ce1e0a649e19e6fe2
Mikhail Vetoshkin <mvetoshkin@gmail.com>
2015-08-03 22:55:56 +0300

    new tests for POST /token endpoint

--- test/tokenController-test.js
+++ test/tokenController-test.js
@@ -2,10 +2,12 @@
 
 // Load modules
 
-var Code   = require('code'),
-    Lab    = require('lab'),
-    //_      = require('lodash'),
-    helper = require('./test-helper');
+var Code    = require('code'),
+    Lab     = require('lab'),
+    // _       = require('lodash'),
+    helper  = require('./test-helper'),
+    Account = require('../lib/models/account'),
+    async   = require('async');
 
 // Declare internals
 
@@ -173,5 +175,111 @@ describe('Token Controller', function () {
       });
     });
 
+    it('should return token for other account when user has admin rights', function (done) {
+      var authUser, token, user;
+
+      var funcs = [
+        function (callback) {
+          return server.methods.createAccount(callback);
+        },
+
+        function (data, callback) {
+          authUser = data.account;
+          token = data.token;
+
+          var attrs = {
+            id: authUser.id,
+            roles: ['user', 'admin']
+          };
+
+          return Account.update(attrs, callback);
+        },
+
+        function (acc, callback) {
+          var request = { method: 'POST', url: '/tokens', headers : helper.authHeader(token) };
+          return server.inject(request, function (res) {
+            return callback(null, res);
+          });
+        },
+
+        function (res, callback) {
+          token = res.result.tokens[0];
+          return server.methods.createAccount(callback);
+        },
+
+        function (data, callback) {
+          user = data.account;
+
+          var payload = {
+            accountId: user.id
+          };
+
+          var request = {
+            method: 'post',
+            url: '/tokens?include=account',
+            payload: payload,
+            headers: helper.authHeader(token)
+          };
+
+          server.inject(request, function (res) {
+            return callback(null, res);
+          });
+        }
+      ];
+
+      return async.waterfall(funcs, function (err, res) {
+        expect(res.statusCode).to.equal(201);
+        expect(res.result.tokens).to.have.length(1);
+        expect(res.result.linked).to.exist();
+        expect(res.result.linked.accounts).to.have.length(1);
+        expect(res.result.linked.accounts[0].id).to.equal(user.id);
+
+        return done();
+      });
+    });
+
+    it('should return token for authenticated user when user does not have admin rights', function (done) {
+      var authUser, token;
+
+      var funcs = [
+        function (callback) {
+          return server.methods.createAccount(callback);
+        },
+
+        function (data, callback) {
+          authUser = data.account;
+          token = data.token;
+
+          return server.methods.createAccount(callback);
+        },
+
+        function (data, callback) {
+          var payload = {
+            accountId: data.account.id
+          };
+
+          var request = {
+            method: 'post',
+            url: '/tokens?include=account',
+            payload: payload,
+            headers: helper.authHeader(token)
+          };
+
+          server.inject(request, function (res) {
+            return callback(null, res);
+          });
+        }
+      ];
+
+      return async.waterfall(funcs, function (err, res) {
+        expect(res.statusCode).to.equal(201);
+        expect(res.result.tokens).to.have.length(1);
+        expect(res.result.linked).to.exist();
+        expect(res.result.linked.accounts).to.have.length(1);
+        expect(res.result.linked.accounts[0].id).to.equal(authUser.id);
+
+        return done();
+      });
+    });
   });
 });
